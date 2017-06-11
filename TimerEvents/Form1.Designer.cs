92c39552c4d714030e09301b34ce6cc0d25c44ae
psoltesz <peter.soltesz92@gmail.com>
2017-06-11 16:15:26 +0200

    first progress bar, yay!

--- TimerEvents/Form1.Designer.cs
+++ TimerEvents/Form1.Designer.cs
@@ -6,6 +6,7 @@
         /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
+        System.Windows.Forms.Timer t;
 
         /// <summary>
         /// Clean up any resources being used.
@@ -28,12 +29,32 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar.Location = new System.Drawing.Point(148, 46);
+            this.progressBar.Name = "progressBar1";
+            this.progressBar.Size = new System.Drawing.Size(303, 38);
+            this.progressBar.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(621, 271);
+            this.Controls.Add(this.progressBar);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar;
     }
 }
 
--- TimerEvents/Form1.cs
+++ TimerEvents/Form1.cs
@@ -16,5 +16,19 @@ namespace TimerEvents
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            t = new System.Windows.Forms.Timer();
+            t.Interval = 1000;
+            t.Tick += new EventHandler(t_Tick);
+            t.Start();
+        }
+
+        void t_Tick(object sender, EventArgs e)
+        {
+            progressBar.Value += 10;
+            if (progressBar.Value >= 100) t.Stop();
+        }
     }
 }
